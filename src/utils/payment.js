3fa08df5c86dc197ba41c582b8cd4cadc792ab32
Yahckman <adeniranajibade2014@gmail.com>
2023-08-10 18:27:38 +0100

    integrating payment

--- src/utils/payment.js
+++ src/utils/payment.js
@@ -1,20 +1,16 @@
-
 import { useFlutterwave, closePaymentModal } from "flutterwave-react-v3";
-import nodemailer from 'nodemailer'
 
-
-export function Payment(price){
-    
+export function Payment(price) {
   const config = {
-    public_key: 'FLWPUBK_TEST-831785520d91c4132b0997dda1a0d8ec-X',
+    public_key: "FLWPUBK_TEST-831785520d91c4132b0997dda1a0d8ec-X",
     tx_ref: Date.now(),
     amount: price,
     currency: "NGN",
     payment_options: "card,mobilemoney,ussd",
     customer: {
-      email: 'adeniran@gmail.com',
-      phone_number: '08146283237',
-      name: 'Adeniran Yaqub',
+      email: "adeniran@gmail.com",
+      phone_number: "08146283237",
+      name: "Adeniran Yaqub",
     },
     text: "Book Now",
     customizations: {
@@ -31,29 +27,27 @@ export function Payment(price){
       callback: async (response) => {
         console.log(response);
         if (response.status === "successful") {
-          try{
-            const transporter= nodemailer.createTransport({
-              service:'Gmail',
-              auth:{
-                user: 'adeniranyaqub565@gmail.com',
-                pass: 'adeniran8146283237'
-              }
-            })
-            const mailOptions ={
-              from:'adeniranyaqub565@gmail.com',
-              to:'adeniranajibade2014@gmail.com',
-              subject:'Payment Successful',
-              text: `u have successfully paid ${price} we wil get your good delivered soon`
-            }
+          // try{
+          //   const transporter= nodemailer.createTransport({
+          //     service:'Gmail',
+          //     auth:{
+          //       user: 'adeniranyaqub565@gmail.com',
+          //       pass: 'adeniran8146283237'
+          //     }
+          //   })
+          //   const mailOptions ={
+          //     from:'adeniranyaqub565@gmail.com',
+          //     to:'adeniranajibade2014@gmail.com',
+          //     subject:'Payment Successful',
+          //     text: `u have successfully paid ${price} we wil get your good delivered soon`
+          //   }
 
-            await transporter.sendMail(mailOptions)
-            window.location.href = "/";
-          }catch(err){
-            console.error("Error sending email:", err);
-            alert("Transaction successful, but email could not be sent.")
-          }
-
-         
+          //   await transporter.sendMail(mailOptions)
+          window.location.href = "/";
+          // }catch(err){
+          //   console.error("Error sending email:", err);
+          //   alert("Transaction successful, but email could not be sent.")
+          // }
         } else {
           alert("Transaction fail");
         }
@@ -62,5 +56,5 @@ export function Payment(price){
       onClose: () => {},
     });
   };
-return handlePayment
+  return handlePayment;
 }
